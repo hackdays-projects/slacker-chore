1ddc48066fea97967eebc8a39789599f850db195
albertlai431 <albertlai631@outlook.com>
2023-01-15 11:22:46 -0500

    group tasks and fix

--- src/components/GroupTasks.js
+++ src/components/GroupTasks.js
@@ -1,9 +1,129 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  VStack,
+  HStack,
+  FormControl,
+  Input,
+  Button,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Select,
+} from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
+import { useEffect, useState } from "react";
+import { ref, set, onValue, off } from "firebase/database";
 
 function GroupTasks({ user, uid, db }) {
+  const [group, setGroup] = useState(null);
+  const [groupName, setGroupName] = useState(null);
+  const [tasks, setTasks] = useState(null);
+  const [input, setInput] = useState("");
+  const [num, setNum] = useState("0");
+  const [occurence, setOccurence] = useState("Daily");
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleNumChange = (e) => {
+    setNum(e);
+  };
+
+  const handleOccurenceChange = (e) => {
+    setOccurence(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const taskId = uuidv4();
+    set(ref(db, "groups/" + group + "/tasks/" + taskId), {
+      name: input,
+      num: num,
+      occurence: occurence,
+    });
+  };
+
+  useEffect(() => {
+    onValue(ref(db, "users/" + uid + "/group"), (snapshot) => {
+      if (snapshot.exists()) {
+        const groupID = snapshot.val();
+        setGroup(groupID);
+        onValue(ref(db, "groups/" + groupID + "/groupName"), (snapshot) => {
+          if (snapshot.exists()) {
+            setGroupName(snapshot.val());
+          }
+        });
+        onValue(ref(db, "groups/" + groupID + "/tasks"), (snapshot) => {
+          if (snapshot.exists()) {
+            setTasks(snapshot.val());
+            console.log(snapshot.val());
+          }
+        });
+      }
+    });
+  }, []);
   return (
     <Box>
-      <Text>Group</Text>
+      <Text>Group Tasks</Text>
+      {group === null ? (
+        <Text>Join a group to see the group tasks</Text>
+      ) : (
+        <Box w="40vw">
+          <Text>{groupName}</Text>
+          {tasks === null ? (
+            <Text>Your group has no tasks</Text>
+          ) : (
+            <VStack borderRadius={10} border="1px" borderColor="gray.400">
+              {Object.keys(tasks).map((task) => {
+                return (
+                  <HStack>
+                    <Text>{tasks[task].name}</Text>
+                    <Text>
+                      every {tasks[task].num} {tasks[task].occurence}
+                    </Text>
+                  </HStack>
+                );
+              })}
+            </VStack>
+          )}
+          <form onSubmit={handleSubmit}>
+            <FormControl>
+              <HStack>
+                <Input
+                  type="text"
+                  placeholder="Add a task"
+                  value={input}
+                  onChange={handleInputChange}
+                  required
+                />
+                <NumberInput value={num} onChange={handleNumChange} required>
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+                <Select
+                  required
+                  placeholder="Select occurence"
+                  onChange={handleOccurenceChange}
+                >
+                  <option value="day">day</option>
+                  <option value="week">week</option>
+                  <option value="month">month</option>
+                </Select>
+                <Button colorScheme="blue" type="submit">
+                  Add
+                </Button>
+              </HStack>
+            </FormControl>
+          </form>
+        </Box>
+      )}
     </Box>
   );
 }
--- src/components/Invites.js
+++ src/components/Invites.js
@@ -1,14 +1,22 @@
 import { Box, Text, Button } from "@chakra-ui/react";
 import { v4 as uuidv4 } from "uuid";
-import {useEffect, useState} from "react";
-import { getDatabase, ref, set, onValue, off, get, child } from "firebase/database";
+import { useEffect, useState } from "react";
+import {
+  getDatabase,
+  ref,
+  set,
+  onValue,
+  off,
+  get,
+  child,
+} from "firebase/database";
 import {
   FormControl,
   FormLabel,
   FormErrorMessage,
   FormHelperText,
   Input,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
 import {
   Table,
@@ -20,55 +28,59 @@ import {
   Td,
   TableCaption,
   TableContainer,
-} from '@chakra-ui/react'
-
+} from "@chakra-ui/react";
 
 function Invites({ user, uid, db }) {
-  const [invites,setInvites] = useState(null)
+  const [invites, setInvites] = useState(null);
 
   useEffect(() => {
     getInvites();
-  }, [])
+  }, []);
 
   const getInvites = () => {
-    onValue(ref(db, "invites/" + user.email.replaceAll(".", " ")), (snapshot) => {
-      if (snapshot.exists()) {
-        setInvites(snapshot.val())
+    onValue(
+      ref(db, "invites/" + user.email.replaceAll(".", " ")),
+      (snapshot) => {
+        if (snapshot.exists()) {
+          setInvites(snapshot.val());
+        }
       }
-    });
-  }
+    );
+  };
   return (
     <TableContainer>
-  <Table variant='striped' colorScheme='teal'>
-    <TableCaption>All Your Invites</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>User</Th>
-        <Th>group name</Th>
-        <Th isNumeric>join</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      { 
-        invites ? 
-        invites.map((invite) => {
+      <Table variant="striped" colorScheme="teal">
+        <TableCaption>All Your Invites</TableCaption>
+        <Thead>
           <Tr>
-            <Th>{invite.inviter}</Th>
-            <Th>{invite.group}</Th>
-            <Th isNumeric><Button colorScheme='blue'>Join Team</Button></Th>
+            <Th>User</Th>
+            <Th>group name</Th>
+            <Th isNumeric>join</Th>
           </Tr>
-          console.log(invite)
-        })
-        :
-        <Tr>
-          <Td>You have no invites</Td>
-          <Td></Td>
-          <Td isNumeric></Td>
-        </Tr>
-      }
-    </Tbody>
-  </Table>
-</TableContainer>    
+        </Thead>
+        <Tbody>
+          {invites ? (
+            invites.map((invite) => {
+              return (
+                <Tr>
+                  <Th>{invite.inviter}</Th>
+                  <Th>{invite.group}</Th>
+                  <Th isNumeric>
+                    <Button colorScheme="blue">Join Team</Button>
+                  </Th>
+                </Tr>
+              );
+            })
+          ) : (
+            <Tr>
+              <Td>You have no invites</Td>
+              <Td></Td>
+              <Td isNumeric></Td>
+            </Tr>
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 }
 
